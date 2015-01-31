1b4d7e8f8b9e688e046e753735bd2dbe926409f6
Mark Ziecina <ziecina@gmail.com>
2015-01-31 03:34:36 -0500

    Task 1 Complete
    I had to make the n of 1 always 0 and 2 always 1.  This made the result
    match up with the expected output.

--- task1.js
+++ task1.js
@@ -1,5 +1,13 @@
+/*
+Mark Ziecina
+SENG 691Z Agile Software Development
+Spring 2015
+Assignment 1
+*/
+
 function fibonacci(n) {
 	if (n < 0) return "Error";
-	else if (n == 0 || n == 1) return n;
-	else fibonacci(n - 1) + fibonacci(n - 2);
+	else if (n == 0 || n == 1) return 0;
+	else if (n == 2) return 1; 
+	else return fibonacci(n - 1) + fibonacci(n - 2);
 }
