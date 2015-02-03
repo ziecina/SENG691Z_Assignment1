73162a74fb4adfa4f0f503ae3eb7a56362d25a84
Mark Ziecina <ziecina@gmail.com>
2015-02-02 21:10:02 -0500

    Task 3 works and tests.

--- task3.js
+++ task3.js
@@ -6,7 +6,21 @@ Assignment 1
 */
 
 function collateTimes(t) {
+	var resultArray = [];
+	var n = 0;
+	var lastDate = new Date(Date.parse(t[0])); 
+	lastDate.setMinutes(lastDate.getMinutes() + 1);
+
+	resultArray[n++] = t[0];
 	
-
-
+	for (var i = 0; i < t.length; i++){
+		var loopDate = new Date(Date.parse(t[i]));
+		if (loopDate > lastDate) {
+			resultArray[n++] = t[i];
+		} 
+		lastDate = loopDate;
+		lastDate.setMinutes(lastDate.getMinutes() + 1);
+		
+	}
+	return resultArray;
 }
